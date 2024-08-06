fc5db391cfa089ed577e9fa52bfd91a546710e39
pratik8 <pratikkalbhor8@gmail.com>
2024-08-06 18:19:01 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import DisplayImages from './Components/displayImages';
+
+import './App.css';
+
+function App() {
+  return <DisplayImages />;
+}
+
+export default App;
