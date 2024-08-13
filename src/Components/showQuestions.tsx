fa2a46ed11a19e096b0080cb476e6c3397bbe52b
pratik8 <pratikkalbhor8@gmail.com>
2024-08-13 15:46:24 +0530

    Update showQuestions.tsx

--- src/Components/showQuestions.tsx
+++ src/Components/showQuestions.tsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from "react";
-import { QUESTIONS } from "../questions";
-import Button from "./button";
+import React, { useState, useEffect, useContext, lazy, Suspense } from 'react';
+import { QUESTIONS } from '../questions';
+import { scoreContext } from '../ContextApi/scoreContext';
 
 const ShowQuestions = () => {
+  const { scoreData } = useContext(scoreContext);
   const [answer, setAnswer] = useState<Object>({});
+  const Button = lazy(() => import('./button'));
+  const ShowScore = lazy(() => import('./showScore'));
 
   useEffect(() => {
-    let answers = JSON.parse(localStorage.getItem("answers"))
+    let answers = JSON.parse(localStorage.getItem('answers'));
     if (answers) {
       setAnswer(answers);
     }
@@ -14,44 +17,73 @@ const ShowQuestions = () => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setAnswer({ ...answer, [e.target.name]: e.target.value });
-  
-  }
+  };
 
   return (
-    <div className='questions-wrapper'>
-      {Object.keys(QUESTIONS).map(key => {
+    <div className="questions-wrapper">
+      <Suspense fallback={<div>Loading...</div>}>
+        <ShowScore answer={answer} score={scoreData} />
+      </Suspense>
+      {Object.keys(QUESTIONS).map((key) => {
         return (
           <>
-            <div>
+            <div className="question-text">
               <label>{QUESTIONS[key]}</label>
-			</div>
-			<div>			
+            </div>
+            <div>
               <input
                 key={key.toString()}
                 type="radio"
                 onChange={handleChange}
                 name={`language_${key}`}
-                checked={Object.keys(answer).length > 0 ? answer[`language_${key}`] === "yes" : false}
+                checked={
+                  Object.keys(answer).length > 0
+                    ? answer[`language_${key}`] === 'yes'
+                    : false
+                }
                 value="yes"
                 data-testid={`language_yes_${key}`}
+                id={`Yes-${key}`}
               />
-              <label>Yes</label>
+              <label
+                htmlFor={`Yes-${key}`}
+                className={`yes-no-label ${
+                  answer[`language_${key}`] === 'yes' ? ' selected' : ''
+                }`}
+              >
+                Yes
+              </label>
               <input
                 key={key.toString()}
                 type="radio"
                 onChange={handleChange}
                 name={`language_${key}`}
-                checked={Object.keys(answer).length > 0 ? answer[`language_${key}`] === "no" : false}
+                checked={
+                  Object.keys(answer).length > 0
+                    ? answer[`language_${key}`] === 'no'
+                    : false
+                }
                 value="no"
                 data-testid={`language_no_${key}`}
+                id={`No-${key}`}
               />
-              <label>No</label>
+              <label
+                htmlFor={`No-${key}`}
+                className={`yes-no-label ${
+                  answer[`language_${key}`] === 'no' ? ' selected' : ''
+                }`}
+              >
+                No
+              </label>
             </div>
-          </>)
+          </>
+        );
       })}
-      <Button answer={answer} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Button answer={answer} />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
 export default ShowQuestions;
