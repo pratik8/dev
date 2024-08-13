f7d270ec5e46c7f94c8dab26982ae38089732ed4
pratik8 <pratikkalbhor8@gmail.com>
2024-08-13 15:54:20 +0530

    Update showScore.tsx

--- src/Components/showScore.tsx
+++ src/Components/showScore.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
 export default function ShowScore({ answer, score }) {
-    return (
-      <div>
-        {(answer && Object.keys(answer).length && score) ? <div data-testid="score">Your Score is : {score}</div> : <></>}
-      </div>
-    )
-  }
+  return (
+    <h3>
+      {answer && Object.keys(answer).length && score ? (
+        <div data-testid="score">Your Score is : {score}</div>
+      ) : (
+        <></>
+      )}
+    </h3>
+  );
+}
