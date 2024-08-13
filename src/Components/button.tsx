2ac858d67a48f9a70c122cb2ed4e83a7dfb589df
pratik8 <pratikkalbhor8@gmail.com>
2024-08-13 15:53:47 +0530

    Update button.tsx

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,29 +1,42 @@
-import { useState } from "react";
-import { QUESTIONS } from "../questions";
-import ShowScore from "./showScore";
+import React, { useState, useContext } from 'react';
+import { QUESTIONS } from '../questions';
+import ShowScore from './showScore';
+import { scoreContext } from '../ContextApi/scoreContext';
 
 export default function Button({ answer }) {
-
-  const [score, setScore] = useState<number>(0);
+  const { addScore } = useContext(scoreContext);
+  // const [score, setScore] = useState<number>(0);
 
   function calculateScore(): void {
     const totalQuestions = Object.keys(QUESTIONS).length;
-    let data = Object.values(answer).reduce((total: number, current): number => {
-      if (current === "yes") {
-        return total + 1;	
-      }
-      return total;
-    }, 0);
-    setScore(data * 100 / totalQuestions);
-	localStorage.setItem("answers", JSON.stringify(answer));
+    let data = Object.values(answer).reduce(
+      (total: number, current): number => {
+        if (current === 'yes') {
+          return total + 1;
+        }
+        return total;
+      },
+      0
+    );
+    addScore((data * 100) / totalQuestions);
+    // setScore((data * 100) / totalQuestions);
+    localStorage.setItem('answers', JSON.stringify(answer));
   }
 
-  return (<>
-    {answer && Object.keys(answer).length === Object.keys(QUESTIONS).length ?
-      <div data-testid="scorebtn">
-        <button className="button" onClick={calculateScore}>Calculate Score</button>
-      </div> 
-      : <></>}
+  return (
+    <>
+      {answer &&
+      Object.keys(answer).length === Object.keys(QUESTIONS).length ? (
+        <div data-testid="scorebtn">
+          <button className="calculate-score" onClick={calculateScore}>
+            Calculate Score
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
 
-    <ShowScore answer={answer} score={score} /></>)
+      {/* <ShowScore answer={answer} score={score} /> */}
+    </>
+  );
 }
